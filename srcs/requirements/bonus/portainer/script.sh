26e607ac0398e2559af783a2decac7a087e90d69
yousseflj9 <youssefmomen@outlook.com>
2025-05-02 14:42:19 +0200

    all gooddd

--- srcs/requirements/bonus/portainer/script.sh
+++ srcs/requirements/bonus/portainer/script.sh
@@ -1,31 +1,60 @@
 #!/bin/bash
 
 
-curl -L https://github.com/portainer/portainer/releases/download/2.16.1/portainer-2.16.1-linux-amd64.tar.gz -o /portainer.tar.gz
+
+# curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# chmod +x /usr/local/bin/docker-compose
+# PORTI_PASS=$(cat /run/secrets/portainer_usr_pass)
+
+# if [ ! -f /data/portainer.db ]; then
+
+# curl -L https://github.com/portainer/portainer/releases/download/2.16.1/portainer-2.16.1-linux-amd64.tar.gz -o /portainer.tar.gz
 
 
-tar xzf /portainer.tar.gz 
+# tar xzf /portainer.tar.gz 
 
-mv -f portainer /usr/local/bin/
-rm /portainer.tar.gz
+# mv -f portainer /usr/local/bin/
+# rm /portainer.tar.gz
 
-chmod +x /usr/local/bin/portainer/portainer
+# chmod +x /usr/local/bin/portainer/portainer
+# # this is the first time run porttainer ::
+# /usr/local/bin/portainer/portainer --admin-password="$PORTI_PASS" --bind=0.0.0.0:7878 --data /data &
 
-PORTI_PASS=$(cat /run/secrets/portainer_usr_pass)
+# sleep 12
 
-if [ ! -f /data/portainer.db ]; then
+# fi
 
-# this is the first time run porttainer ::
-/usr/local/bin/portainer --admin-password="$PORTI_PASS" --bind=0.0.0.0:7878 --data /data &
+# # tail -f /dev/null
 
-sleep 12
 
-fi
+# exec /usr/local/bin/portainer/portainer --bind=0.0.0.0:7878 --data /data
 
-# tail -f /dev/null
+
+
+# !/bin/bash
+
+RAW_PASS=$(cat /run/secrets/portainer_usr_pass)
+PORTI_PASS=$(htpasswd -nbBC 10 "" "$RAW_PASS" | tr -d ':\n')
+# Install docker-compose if needed
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+chown -R 1000:1000 /data
+# Download Portainer if not already set up
+if [ ! -f /data/portainer.db ]; then
+  curl -L https://github.com/portainer/portainer/releases/download/2.16.1/portainer-2.16.1-linux-amd64.tar.gz -o /portainer.tar.gz
+  tar xzf /portainer.tar.gz
+  mv -f portainer /usr/local/bin/
+  rm /portainer.tar.gz
+  chmod +x /usr/local/bin/portainer/portainer
 
-exec /usr/local/bin/portainer/portainer --bind=0.0.0.0:7878 --data /data
+  # First-time launch to create admin
+  /usr/local/bin/portainer/portainer \
+    --admin-password="$PORTI_PASS" \
+    --bind=0.0.0.0:7878 \
+    --data /data
 
+else
+  # Just launch normally
+  exec /usr/local/bin/portainer/portainer --bind=0.0.0.0:7878 --data /data
+fi
